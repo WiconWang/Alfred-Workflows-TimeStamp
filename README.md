adfc3bee5706e87071e0e366e726885c0cee6785
WiconWang <WiconWang@gmail.com>
2017-03-01 18:43:06 +0800

    添加插件主体

---
+++ README.md
@@ -0,0 +1,9 @@
+# Alfred Workflows TimeStamp
+
+此 workflows 可以转换时间与时间戳，同时显示与当前时间的距离
+命令
+time now 显示当前时间与时间戳
+time 1488888888 时间戳转为时间
+time 2017-03-07 20:14:48 时间转为时间戳，对时间格式要求不严谨
+
+可以手动更新 TimeStamp更改时区
---
+++ TimeStamp.php
@@ -0,0 +1,40 @@
+<?php
+ini_set('date.timezone','Asia/Shanghai');
+require_once('workflows.php');
+
+class TimeStamp{
+
+	public function getTimeStamp($query){
+		$workflows = new Workflows();
+		$now = time();
+		$query = trim($query);
+		if ($query == 'now') {
+			$workflows->result(	$query,
+				time(),
+				'当前时间戳：'.time(),
+				'当前时间：'.date('Y-m-d H:i:s',time()),
+				'icon.png',false);
+		}elseif (floor($query)==$query) {
+			$cle = $query-time();
+			$d = floor($cle/3600/24);
+			$h = floor(($cle%(3600*24))/3600);
+			$m = floor(($cle%(3600*24))%3600/60);
+			$s = floor(($cle%(3600*24))%60);
+			$workflows->result(	$query,
+				date('Y-m-d H:i:s',$query),
+				'目标时间：'.date('Y-m-d H:i:s',$query),
+				"当前时间差 $d 天 $h 小时 $m 分 $s 秒",
+				'icon.png',false);
+		}else{
+			$workflows->result(	$query,
+				strtotime($query),
+				'目标时间戳：'.strtotime($query),
+				'当前时间差'.strtotime($query)-time(),
+				'icon.png',false);
+
+		}
+
+		echo $workflows->toxml();
+	}
+
+}
