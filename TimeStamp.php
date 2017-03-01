6235fbbb14c5f07a78aa0a285e633adbd230fa15
WiconWang <WiconWang@gmail.com>
2017-03-01 19:21:09 +0800

    Fixed:有个小 BUG

--- TimeStamp.php
+++ TimeStamp.php
@@ -14,7 +14,7 @@ class TimeStamp{
 				'当前时间戳：'.time(),
 				'当前时间：'.date('Y-m-d H:i:s',time()),
 				'icon.png',false);
-		}elseif (floor($query)==$query) {
+		}elseif ($query == (string)intval($query)) {
 			$cle = $query-time();
 			$d = floor($cle/3600/24);
 			$h = floor(($cle%(3600*24))/3600);
@@ -26,15 +26,15 @@ class TimeStamp{
 				"当前时间差 $d 天 $h 小时 $m 分 $s 秒",
 				'icon.png',false);
 		}else{
+			$query2 = strtotime($query)-time();
 			$workflows->result(	$query,
 				strtotime($query),
 				'目标时间戳：'.strtotime($query),
-				'当前时间差'.strtotime($query)-time(),
+				'当前时间差：'.$query2,
 				'icon.png',false);
-
 		}
-
 		echo $workflows->toxml();
+
 	}
 
 }
