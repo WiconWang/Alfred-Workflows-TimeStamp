77cd7d77e0573432002a3b19d3f9cbbf0d4679da
wicon wang <wiconwang@gmail.com>
2021-03-31 14:45:38 +0800

    Improved： 支持毫秒级时间

--- TimeStamp.php
+++ TimeStamp.php
@@ -8,44 +8,87 @@ class TimeStamp{
         return $ret !== FALSE && $ret != -1;
     }
 
+
+	private function getDifference($queryStamp,$nowStamp){
+		$cle = $queryStamp-$nowStamp;
+        if ($cle > 0) {
+            $d = floor($cle/3600/24);
+            $h = floor(($cle%(3600*24))/3600);
+            $m = floor(($cle%(3600*24))%3600/60);
+            $s = floor(($cle%(3600*24))%60);
+        }elseif ($cle < 0) {
+            $d = ceil($cle/3600/24);
+            $h = ceil(($cle%(3600*24))/3600);
+            $m = ceil(($cle%(3600*24))%3600/60);
+            $s = ceil(($cle%(3600*24))%60);
+        }else {
+            $d = 0;
+            $h = 0;
+            $m = 0;
+            $s = 0;
+        }
+		return "当前时间差 $d 天 $h 小时 $m 分 $s 秒";
+
+	}
+
     public function getTimeStamp($query){
         $workflows = new Workflows();
         $now = time();
         $query = trim($query);
 
         if ($query == 'now') {
-            $workflows->result( $query,
-                time(),
-                '当前时间戳：'.time(),
-                '当前时间：'.date('Y-m-d H:i:s',time()),
-                'icon.png',false);
+            $workflows->result(	$query,
+            date('Y年m月d日 H时i分s秒',time()),
+            "系统时间：". date('Y年m月d日 H时i分s秒',time()),
+            '' ,
+            'icon.png',false);
+
+        $workflows->result(	$query,
+            date('Y-m-d H:i:s',time()),
+            "系统时间：". date('Y-m-d H:i:s',time()),
+            '' ,
+            'icon.png',false);
+
+            $workflows->result(	$query,
+            date('YmdHis',time()),
+            "系统时间：". date('YmdHis',time()),
+            '' ,
+            'icon.png',false);
+
+        $workflows->result(	$query,
+            time()*1000,
+            "毫秒时间戳：". time()*1000,
+            '',
+            'icon.png',false);
+
+        $workflows->result(	$query,
+            time(),
+            "系统时间戳：". time(),
+            '',
+            'icon.png',false);
 
             echo $workflows->toxml();
         }
         if(is_numeric($query)){
             $cle = $query-$now;
-            if ($cle > 0) {
-                $d = floor($cle/3600/24);
-                $h = floor(($cle%(3600*24))/3600);
-                $m = floor(($cle%(3600*24))%3600/60);
-                $s = floor(($cle%(3600*24))%60);
-            }elseif ($cle < 0) {
-                $d = ceil($cle/3600/24);
-                $h = ceil(($cle%(3600*24))/3600);
-                $m = ceil(($cle%(3600*24))%3600/60);
-                $s = ceil(($cle%(3600*24))%60);
-            }else {
-                $d = 0;
-                $h = 0;
-                $m = 0;
-                $s = 0;
-            }
+            if(preg_match('/^\d{1,10}$/',$query)){
             $workflows->result( $query,
                 date('Y-m-d H:i:s',$query),
                 '目标时间：'.date('Y-m-d H:i:s',$query),
-                "当前时间差 $d 天 $h 小时 $m 分 $s 秒",
+				$this->getDifference( $query, time()),
                 'icon.png',false);
-            echo $workflows->toxml();
+                echo $workflows->toxml();
+            }
+
+            if(preg_match('/^\d{11,13}$/',$query)){
+                $resQuery = $query/1000;
+                $workflows->result( $resQuery,
+                    date('Y-m-d H:i:s',$resQuery),
+                    '目标时间：'.date('Y-m-d H:i:s',$resQuery),
+                    $this->getDifference( $resQuery, time()),
+                    'icon.png',false);
+                    echo $workflows->toxml();
+                }
         }
         if ($this->isDateTime($query)) {
             $workflows->result( $query,
@@ -53,6 +96,11 @@ class TimeStamp{
                 '目标时间戳：'.strtotime($query),
                 '与当前时间戳差：'.(strtotime($query)-$now).'秒',
                 'icon.png',false);
+                $workflows->result( $query,
+                    strtotime($query) * 1000,
+                    '目标时间毫秒戳：'.strtotime($query) * 1000,
+                    '与当前时间毫秒戳差：'. ((strtotime($query)-$now) * 1000 ) .'毫秒' ,
+                    'icon.png',false);
             echo $workflows->toxml();
         }
         exit;
